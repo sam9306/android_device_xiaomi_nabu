ca6738fbd5ecdc4a70b8747564f69047628cbd9d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-07 21:55:48 +0200

    nabu: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: 21051182G Script version: 2.3.0 # Conflicts: #	BoardConfig.mk #	omni_nabu.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,6 +10,7 @@ DEVICE_PATH := device/xiaomi/nabu
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
+<<<<<<< HEAD
 # A/B
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \
@@ -20,19 +21,27 @@ AB_OTA_PARTITIONS += \
     odm
 BOARD_USES_RECOVERY_AS_BOOT := true
 
+=======
+>>>>>>> b9ef692 (nabu: Initial TWRP device tree)
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
+<<<<<<< HEAD
 TARGET_CPU_ABI2 := 
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_VARIANT_RUNTIME := generic
+=======
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+>>>>>>> b9ef692 (nabu: Initial TWRP device tree)
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
+<<<<<<< HEAD
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a9
 
 # APEX
@@ -70,12 +79,50 @@ BOARD_SUPER_PARTITION_SIZE := 9126805504 # TODO: Fix hardcoded value
 BOARD_SUPER_PARTITION_GROUPS := xiaomi_dynamic_partitions
 BOARD_XIAOMI_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext product vendor odm
 BOARD_XIAOMI_DYNAMIC_PARTITIONS_SIZE := 9122611200 # TODO: Fix hardcoded value
+=======
+TARGET_BOARD_SUFFIX := _64
+TARGET_USES_64_BIT_BINDER := true
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := nabu
+
+# File systems
+BOARD_HAS_LARGE_FILESYSTEM := true
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 134217728 # This is the maximum known partition size, but it can be higher, so we just omit it
+BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+TARGET_COPY_OUT_VENDOR := vendor
+
+# A/B
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS += \
+    boot \
+    system \
+    vendor
+
+TW_INCLUDE_REPACKTOOLS := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE := None
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
+BOARD_BOOTIMG_HEADER_VERSION := 3
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_SOURCE := kernel/xiaomi/nabu
+TARGET_KERNEL_CONFIG := nabu_defconfig
+>>>>>>> b9ef692 (nabu: Initial TWRP device tree)
 
 # Platform
 TARGET_BOARD_PLATFORM := msmnile
 
 # Recovery
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+<<<<<<< HEAD
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
@@ -85,6 +132,8 @@ VENDOR_SECURITY_PATCH := 2021-08-01
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+=======
+>>>>>>> b9ef692 (nabu: Initial TWRP device tree)
 
 # Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
@@ -97,4 +146,7 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+<<<<<<< HEAD
 TW_INCLUDE_REPACKTOOLS := true
+=======
+>>>>>>> b9ef692 (nabu: Initial TWRP device tree)
